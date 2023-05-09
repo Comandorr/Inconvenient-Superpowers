f1803f7ece8ab6d7f94a6bcc7ee4eda4c9f18d3f
Comandorr <Plane135790@gmail.com>
2023-05-09 08:45:49 +0300

    animated hero

---
+++ hero.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="hero" tilewidth="48" tileheight="48" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="48" height="48" source="hero_idle/tile000.png"/>
+ </tile>
+ <tile id="1">
+  <image width="48" height="48" source="hero_idle/tile001.png"/>
+ </tile>
+ <tile id="2">
+  <image width="48" height="48" source="hero_idle/tile002.png"/>
+ </tile>
+ <tile id="3">
+  <image width="48" height="48" source="hero_idle/tile003.png"/>
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import arcade
 import random
 from pyglet.math import Vec2
 
-class Player(arcade.Sprite):
-	def __init__(self, screen):
-		super().__init__('character.png', scale=2, center_x = screen.width/2, center_y = 150)
+class Player(arcade.AnimatedTimeBasedSprite):
+	def __init__(self, filename, screen, scale):
+		super().__init__(filename, scale=2, center_x = screen.width/2, center_y = 150)
 		self.keys = {'w':False, 's':False, 'a':False, 'd':False}
 		self.screen = screen
-		self.speed = 2
-		self.jump_speed = 7
-		self.superpower = None
+		self.speed = 4
+		self.jump_speed = 8
+		self.timer = 0
+		self.timer_len = 1
+		self.superpower = 'none'
 		self.superlist = ['superspeed', 'antigravity', 'teleportation', 'earthquake']
 
-	def update(self):
+	def update(self, delta_time = 1/60):
+		if self.superpower == 'teleportation':
+			self.timer += delta_time
+			if self.timer >= self.timer_len:
+				self.timer = 0
+				self.center_x += random.randint(-500, 500)
+				self.center_y = 150+random.randint(0, 200)
+			
 		if self.superpower == 'superspeed':
-			self.speed = 20
+			self.speed = 35
 		else:
-			self.speed = 2
+			self.speed = 4
 
 		if self.superpower == 'antigravity':
 			self.screen.physics_engine.gravity_constant = random.randint(-1, 1)/10
@@ -37,25 +46,28 @@ class Player(arcade.Sprite):
 
 	def get_superpower(self):
 		self.superpower = random.choice(self.superlist)
-
+		if self.superpower == 'teleportation':
+			self.timer_len = 10000
 
 class Game(arcade.Window):
 	def __init__(self):
 		super().__init__(1280, 720, vsync=True)
 		arcade.set_background_color((40, 44, 56))
-		self.player = Player(self)
 
 		self.camera = arcade.Camera()
 		lp = {
             "Platforms": {"use_spatial_hash": True},
+			"Hero":{"custom_class": Player, "custom_class_args": {"screen": self}},
         }
 		self.tile_map = None
 		self.tile_map = arcade.load_tilemap('map.tmx', 1.5, lp)
 		self.scene = arcade.Scene.from_tilemap(self.tile_map)
-		self.scene.add_sprite('Player', self.player)
+		self.player = self.scene["Hero"][0]
+		self.scene.add_sprite('Player',self.player)
 		self.physics_engine = arcade.PhysicsEnginePlatformer(self.player, self.scene['Platforms'], gravity_constant=0.5)
 		
 	def on_update(self, delta_time):
+		self.player.update_animation()
 		self.player.update()
 		self.physics_engine.update()
 		position = Vec2(self.player.center_x - self.width / 2, 0)
@@ -63,11 +75,6 @@ class Game(arcade.Window):
 		if self.player.superpower == 'earthquake':
 			self.camera.shake(Vec2(random.randint(-2, 2), random.randint(-5, 5)), speed=1, damping=0.95)
 			self.camera.update()
-		if self.player.superpower == 'teleportation':
-			self.player.left = random.randint(int(self.camera.position[0]), int(self.camera.position[0]+self.width-self.player.width))
-			self.player.bottom = random.randint(0, self.height-self.player.height)
-		
-
 
 	def on_draw(self):
 		self.camera.use()
